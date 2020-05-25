a8a3f3bf1bf7c224f2631a4c3bae8348de292dab
Dalton Teixeira <dalton.silveira@gmail.com>
2020-05-25 16:39:00 -0300

    Add files via upload

---
+++ api/api.py
@@ -0,0 +1,52 @@
+import flask
+from flask import jsonify
+from flask import request
+import json
+
+app = flask.Flask(__name__)
+
+db = {}
+try:
+    with open("db.json", "r+") as db_file:
+        db_saved_data = json.loads(db_file.read())
+        for key in db_saved_data:
+            db[key] = db_saved_data[key]
+except:
+    pass
+
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify(db)
+
+@app.route('/<id>', methods=['GET'])
+def get_by_id(id):
+    return jsonify(db[id])
+
+
+@app.route('/', methods=['POST'])
+def post():
+    request_json = request.get_json()
+    response = {"subtotal": 0, "delivery": 0, "items": []}
+
+    for item in request_json:
+        db_item = db[item.get("id")]
+        item_subtotal = float(db_item["price"]) * int(item["quantity"])
+        item_delivery = float(db_item["delivery"]) * int(item["quantity"])
+        response["subtotal"] = float(response["subtotal"]) + item_subtotal
+        response["items"].append({"id": item["id"], "delivery": item_delivery, "subtotal": item_subtotal})
+        response["delivery"] = response["delivery"] + item_delivery
+    response["total"] = response["subtotal"] + response["delivery"]
+    return jsonify(response)
+
+
+@app.route('/', methods=['PUT'])
+def put():
+    request_json = request.get_json()
+    db[request_json.get("id")] = request_json
+    with open("db.json", "w+") as _db_file:
+        _db_file.write(json.dumps(db))
+    return jsonify(request_json)
+
+
+app.run()
